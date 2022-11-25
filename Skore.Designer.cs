32a395ae3ed52d323543a1a14e8e5f6a043d9a22
brenekv.03 <brenekv.03@A321-15>
2022-11-25 09:03:55 +0100

    Vytvoření dataGrid

--- Skore.Designer.cs
+++ Skore.Designer.cs
@@ -32,6 +32,13 @@
             this.button1 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Jméno = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.MaximalniDelkaHry = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Skóre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DelkaHry = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.HerniMod = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -62,11 +69,51 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Jméno,
+            this.MaximalniDelkaHry,
+            this.Skóre,
+            this.DelkaHry,
+            this.HerniMod});
+            this.dataGridView1.Location = new System.Drawing.Point(34, 441);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(542, 150);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // Jméno
+            // 
+            this.Jméno.HeaderText = "Jméno";
+            this.Jméno.Name = "Jméno";
+            // 
+            // MaximalniDelkaHry
+            // 
+            this.MaximalniDelkaHry.HeaderText = "Maximální délka hry";
+            this.MaximalniDelkaHry.Name = "MaximalniDelkaHry";
+            // 
+            // Skóre
+            // 
+            this.Skóre.HeaderText = "Skóre";
+            this.Skóre.Name = "Skóre";
+            // 
+            // DelkaHry
+            // 
+            this.DelkaHry.HeaderText = "Délka Hry";
+            this.DelkaHry.Name = "DelkaHry";
+            // 
+            // HerniMod
+            // 
+            this.HerniMod.HeaderText = "Herní Mód";
+            this.HerniMod.Name = "HerniMod";
+            // 
             // Skore
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(657, 750);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
@@ -74,6 +121,7 @@
             this.Name = "Skore";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Tetris";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -83,5 +131,11 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Jméno;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MaximalniDelkaHry;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Skóre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DelkaHry;
+        private System.Windows.Forms.DataGridViewTextBoxColumn HerniMod;
     }
 }
