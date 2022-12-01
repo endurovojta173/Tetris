3f0d33e4ec786853ef4d72d4d08ff7d6cecb07b5
brenekv.03 <brenekv.03@A321-15>
2022-12-01 11:02:23 +0100

    Vytvoření classy pro ukládání - jenom nakopírování funkcí

--- UlozeniDat.cs
+++ UlozeniDat.cs
@@ -26,5 +26,46 @@ namespace Tetris
             }
             sw.Write(herniMod+";");
         }
+
+        public static void SaveSkore()
+        {
+            FileStream fs = new FileStream(@"../../skoreHry.txt", FileMode.OpenOrCreate, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+            string radek = "";
+            char[] separators = { ';' };
+            while (!sr.EndOfStream)
+            {
+                string polozka = sr.ReadLine();
+                if (polozka != "#")
+                {
+                    if (polozka == "True")
+                    {
+                        radek += " Časový mód";
+                    }
+                    else if (polozka == "False")
+                    {
+                        radek += " Nekonečný mód";
+                    }
+                    else
+                    {
+                        radek += polozka;
+                    }
+                }
+                else
+                {
+                    FileStream fs1 = new FileStream(@"../../skore.txt", FileMode.Open, FileAccess.Write);
+                    StreamWriter sw = new StreamWriter(fs1);
+                    string[] splitRadek = radek.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+                    radek = splitRadek[0] + ";" + splitRadek[1] + ";" + splitRadek[2] + ";" + splitRadek[3] + ";" + splitRadek[4] + ";" + splitRadek[5];
+                    sw.BaseStream.Seek(0, SeekOrigin.End);
+                    sw.WriteLine(radek);
+                    sw.Close();
+                    fs1.Close();
+                }
+            }
+            fs.Close();
+            FileStream fs2 = new FileStream(@"../../skoreHry.txt", FileMode.Create, FileAccess.Write);
+            fs2.Close();
+        }
     }
 }
